fbba6d7e5e03bafe52aa2ab99e43b3dfba2b42d5
Rizan Zaky <rizan149@gmail.com>
2020-10-17 18:58:12 +0530

    load dates for month

--- src/Holidays/MainPage.xaml.cs
+++ src/Holidays/MainPage.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Holidays
@@ -13,6 +11,34 @@ namespace Holidays
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainPageViewModel();
+            //SetBinding(BindingContextProperty, new Binding());
         }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            if (BindingContext is MainPageViewModel vm)
+            {
+                var days = calendarView.Children.Skip(7).OfType<Label>().ToList();
+
+                var count = 1;
+                var dayOfWeek = vm.ActiveMonth.DayOfWeek == 0 ? 7 : (int) vm.ActiveMonth.DayOfWeek;
+                var daysInMonth = DateTime.DaysInMonth(vm.ActiveMonth.Year, vm.ActiveMonth.Month) + dayOfWeek - 1;
+                for (var i = dayOfWeek - 1; i < daysInMonth; i++)
+                {
+                    days[i].Text = $"{count}";
+                    count++;
+                }
+            }
+        }
+    }
+
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public DateTime ActiveMonth { get; set; } = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+        
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
